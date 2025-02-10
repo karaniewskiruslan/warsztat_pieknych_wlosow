7f8d5c08d4cd1fdae213d9ac788d144c79402539
Rusłan Karaniewski <karane88vka@gmail.com>
2025-02-10 23:04:53 +0100

    Initialize repository

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes } from 'react-router'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <section data-header className="font">
+      Warsztat
+    </section>
+  );
+};
+
+export default Header;
