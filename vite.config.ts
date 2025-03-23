ce352b62f32126118e010ccbfe5165a3dff9df29
Rusłan Karaniewski <karane88vka@gmail.com>
2025-03-23 23:32:45 +0100

    Hope it's work!

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/warsztat-peknych-wlosow/',
+  base: "/warsztat_pieknych_wlosow/",
   plugins: [react(), tailwindcss()],
 });
