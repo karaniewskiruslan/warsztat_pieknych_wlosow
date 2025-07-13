21dedba40254e7e56ccff041d878c9432fe4c8f3
Rusłan Karaniewski <karane88vka@gmail.com>
2025-07-13 15:39:54 +0200

    temporary rename Contact

--- src/components/RoutesContainer.tsx
+++ src/components/RoutesContainer.tsx
@@ -6,7 +6,7 @@ import Booking from "./Booking/Booking";
 import EmptyPage from "./EmptyPage/EmptyPage";
 import Admin from "./Admin/Admin";
 import Master from "./Masters/Master/Master";
-import Contact from "./Contact/Contact";
+import Contact from "./_TempContact/Contact";
 
 const RoutesContainer = () => {
   return (
