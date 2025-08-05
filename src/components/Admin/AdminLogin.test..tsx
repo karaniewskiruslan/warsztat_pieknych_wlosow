dfe6d8f4f737c0525c51205d1eeef6e06a2ba7af
Rusłan Karaniewski <karane88vka@gmail.com>
2025-08-05 18:55:37 +0200

    Book the date page update

---
+++ src/components/Admin/AdminLogin.test..tsx
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { renderWithRouter } from "../../renderWithRouter";
+import Admin from "./Admin";
+import { screen } from "@testing-library/react";
+
+describe("Admin", () => {
+  it("Should be rendered", () => {
+    renderWithRouter(<Admin />, "/admin");
+
+    expect(screen.getByTestId("admin")).toBeInTheDocument();
+  });
+});
--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,7 +1,37 @@
+import booksy from "/img/socials/booksy.svg";
+
 const Booking = () => {
   return (
-    <div data-testid="booking">
+    <div data-testid="booking" className="space-y-4">
       <h1>Jak do nas się zapisać</h1>
+      <section className="space-y-2">
+        <hgroup>
+          <h2>Na booksy</h2>
+          <p>
+            Wybierz najbardziej dogodny dzień i godzinę, a my przemienimy tę
+            chwilę w pełną zachwytu podróż do świata piękna i relaksu.
+          </p>
+        </hgroup>
+        <a
+          href="https://booksy.com/pl-pl/172573_warsztat-pieknych-wlosow_fryzjer_3_warszawa?do=invite&_branch_match_id=1430280485439261899&utm_medium=merchant_customer_invite&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXT07J0UvKz88urtRLzs%2FVz48Mc03yNfGz8EuyrytKTUstKsrMS49PKsovL04tsnXLBIrlVwAAeWwQpz0AAAA%3D"
+          target="blank"
+          className="flex w-fit items-center gap-1 border-b text-xl font-bold"
+        >
+          <span>Umów się już teraz</span>
+          <img src={booksy} alt="booksy" className="size-5" />
+        </a>
+      </section>
+
+      <section className="space-y-2">
+        <hgroup>
+          <h2>JUŻ WKRÓTCE - Umówienie wizyty na stronie WPW</h2>
+          <p>
+            Szykują się zmiany na naszej stronie, i wszystko dla twojej wygody!
+            Będzie gotowe wkrótce.
+          </p>
+        </hgroup>
+        <p></p>
+      </section>
     </div>
   );
 };
