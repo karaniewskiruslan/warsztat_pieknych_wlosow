ec83be03079d1c4a15567663010d02ee24d36707
karaniewskiruslan <113471991+karaniewskiruslan@users.noreply.github.com>
2025-08-15 16:24:55 +0000

    Deploying to gh-pages from @ karaniewskiruslan/warsztat_pieknych_wlosow@1370ceabdee56d5e4ecd1f5b45de9013ea190bcb 🚀

--- assets/Layout-DLaytEBa.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as a}from"./index-eXgjX4Vq.js";const x=()=>t.jsx("main",{className:"font-poppins tablet:px-15 mx-auto w-full max-w-7xl px-8 py-12",children:t.jsx(a,{})});export{x as default};
